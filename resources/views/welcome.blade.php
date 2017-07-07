f2b41228ad3e90f2a69bc50cfb3a6e4af5d2ba16
brobert <robert.blonski+git@gmail.com>
2017-07-07 17:38:04 +0200

    added react route to main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,7 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/react">React</a>
                 </div>
             </div>
         </div>
