60166d6486b860ad26c6eca766dd378917e25631
rb5169 <rb5169@hurra.com>
2017-07-07 15:39:40 +0200

    added possibility to expand  task details

--- resources/react/components/kanban/task.js
+++ resources/react/components/kanban/task.js
@@ -2,27 +2,44 @@ import React from 'react';
 
 class TaskComponent extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            showDetails: false,
+        };
+        
+        this.toggleDetails = this.toggleDetails.bind(this);
+    }
+
+    toggleDetails() {
+        console.info('toggle show details');
+        this.setState({
+            showDetails: !this.state.showDetails,
+        });
+    }
 
 
-
-  render() {
-      console.info();
-      const task = this.props.task;
-    return (
-        <div className="task-component well">
-            <h5 className="task-title">{task.name}</h5>
-            <div className="">
-                {   task.description
-                    ?(<div className="">{task.description}</div>)
+    render() {
+        const task = this.props.task;
+        return (
+            <div className="task-component well">
+                <h5 className="task-title" onClick={this.toggleDetails}>{task.name}</h5>
+                { 
+                    this.state.showDetails
+                    ? (
+                        <div className="">
+                            {   task.description
+                                ?(<div className="">{task.description}</div>)
+                                : null
+                            }
+                        </div>
+                    )
                     : null
                 }
             </div>
-        </div>
-    );
-  }
+        );
+    }
 
 }
 
